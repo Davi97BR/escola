dcc6e1d7164bf3f8d76b4ede429a206e3c8f0837
Davi <davi.1997@outlook.com>
2020-08-07 09:49:17 -0300

    modificando codigo para testar o git

--- src/escola/Escola.java
+++ src/escola/Escola.java
@@ -17,67 +17,89 @@ public class Escola {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        public ArrayList <String> nome;
+        public ArrayList <Integer> ra;
+        public ArrayList <Double> nota1, nota2, nota3, nota4;
 
-        // TODO code application logic here
+        public  Aluno (){
+            nome = new ArrayList<>();
+            ra = new ArrayList<>();
+            nota1 = new ArrayList<>();
+            nota2 = new ArrayList<>();
+            nota3 = new ArrayList<>();
+            nota4 = new ArrayList<>();
+            
+        
+       //modificando o codigo pra testar o git
+
+     
     }
     
-    public class Turma {
-    Aluno Alunos[] = new Aluno[5];
+    
+    
+    public class Aluno extends Escola {
+        public ArrayList<String> nome;
+        public ArrayList <Integer> ra;
+        public ArrayList <Double> nota1, nota2, nota3, nota4;
+
+        public  Aluno (){
+            nome = new ArrayList<>();
+            ra = new ArrayList<>();
+            nota1 = new ArrayList<>();
+            nota2 = new ArrayList<>();
+            nota3 = new ArrayList<>();
+            nota4 = new ArrayList<>();
+        }
+
+        public void addNome(){
+            Scanner entrada = new Scanner (System.in);
+            System.out.println("Informe nome do aluno:");
+            String aNome = entrada.nextLine();
+            nome.add(aNome);        
+        }
+        public void addRa(){
+            Scanner entrada = new Scanner (System.in);
+            System.out.println("Informe RA do aluno:");
+            int aRa = entrada.nextInt();
+            ra.add(aRa);        
+        }
+        public void addNota1(){
+            Scanner entrada = new Scanner (System.in);
+            System.out.println("Informe nome do aluno:");
+            double aNota1 = entrada.nextDouble();
+            nota1.add(aNota1);        
+        }
+        public void addNota2(){
+            Scanner entrada = new Scanner (System.in);
+            System.out.println("Informe nome do aluno:");
+            double aNota2 = entrada.nextDouble();
+            nota2.add(aNota2);        
+        }
+        public void addNota3(){
+            Scanner entrada = new Scanner (System.in);
+            System.out.println("Informe nome do aluno:");
+            double aNota3 = entrada.nextDouble();
+            nota3.add(aNota3);        
+        }
+        public void addNota4(){
+            Scanner entrada = new Scanner (System.in);
+            System.out.println("Informe nome do aluno:");
+            double aNota4 = entrada.nextDouble();
+            nota4.add(aNota4);        
+        }
+        public  void listaAlunos(){
+        for(int i =0; i < nome.size(); i++){
+                System.out.println("Insira nome do"+i+1+"º aluno.");
+                System.out.println(""+nome.get(i));
+            }
+    
+        }
+    }    
+    
+    
+    public class Turma extends Escola {
+    }
     
 }
     
-    public class Aluno {
-    public ArrayList<String> nome;
-    public ArrayList <Integer> ra;
-    public ArrayList <Double> nota1, nota2, nota3, nota4;
-    
-    public  Aluno (){
-        nome = new ArrayList<>();
-        ra = new ArrayList<>();
-        nota1 = new ArrayList<>();
-        nota2 = new ArrayList<>();
-        nota3 = new ArrayList<>();
-        nota4 = new ArrayList<>();
-    }
-    
-    public void addNome(){
-        Scanner entrada = new Scanner (System.in);
-        System.out.println("Informe nome do aluno:");
-        String aNome = entrada.nextLine();
-        nome.add(aNome);        
-    }
-    public void addRa(){
-        Scanner entrada = new Scanner (System.in);
-        System.out.println("Informe RA do aluno:");
-        int aRa = entrada.nextInt();
-        ra.add(aRa);        
-    }
-    public void addNota1(){
-        Scanner entrada = new Scanner (System.in);
-        System.out.println("Informe nome do aluno:");
-        double aNota1 = entrada.nextDouble();
-        nota1.add(aNota1);        
-    }
-    public void addNota2(){
-        Scanner entrada = new Scanner (System.in);
-        System.out.println("Informe nome do aluno:");
-        double aNota2 = entrada.nextDouble();
-        nota2.add(aNota2);        
-    }
-    public void addNota3(){
-        Scanner entrada = new Scanner (System.in);
-        System.out.println("Informe nome do aluno:");
-        double aNota3 = entrada.nextDouble();
-        nota3.add(aNota3);        
-    }
-    public void addNota4(){
-        Scanner entrada = new Scanner (System.in);
-        System.out.println("Informe nome do aluno:");
-        double aNota4 = entrada.nextDouble();
-        nota4.add(aNota4);        
-    }
-    
-}
-    
-}
+
